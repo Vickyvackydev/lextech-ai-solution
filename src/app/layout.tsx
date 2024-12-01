a88f9074f8ef0b86adc2ad3a7ef593e6bab5aebc
Vickyvackydev <vickyvacky5@gmail.com>
2024-12-01 06:34:02 -0800

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "LexTech AI",
   description: "An AI Assistant dedicated to case management",
   icons: {
